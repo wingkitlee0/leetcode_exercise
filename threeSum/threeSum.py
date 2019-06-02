e93227f5c49371d55ab242632d0a3711bc602d57
Kit Lee <wingkitlee0@outlook.com>
2019-06-01 22:42:22 -0500

    update

--- threeSum/threeSum.py
+++ threeSum/threeSum.py
@@ -13,11 +13,14 @@ class Solution:
         """
         return self.three_sum_1(nums, target)
 
-    def three_sum_1(self, nums, target):
+    @staticmethod
+    def three_sum_1(nums, target):
         """
-        3sum, first try
+        3sum, first try, passed leetcode
 
         x+y+z = target => y+z = w = target-x
+
+        - use set to remove duplicates
         """
         nums = sorted(nums) # O(n log n)
         print("sorted nums = {}".format(nums))
@@ -31,22 +34,76 @@ class Solution:
                 continue
             
             w = target - x
-            print("w = {}".format(w))
+            #print("w = {}".format(w))
             start = i+1
             end = N - 1
-            while end > start and nums[start]+nums[end] != w:
+            while end > start:
                 if nums[start]+nums[end] > w:
                     end -= 1
                 if nums[start]+nums[end] < w:
                     start += 1
-                
+                if end > start and nums[start]+nums[end] == w:
+                    entry = tuple(sorted([nums[start],nums[end],x]))
+                    #entry = [x, nums[start], nums[end]]
+                    print(entry)
+                    res.add(entry)
+                    #res.append(entry)
 
-                print(start, end, nums[start], nums[end])
+                    end -= 1
+                    start += 1
+
             
-            if end > start and nums[start]+nums[end] == w:
-                res.add(tuple(sorted([nums[start],nums[end],x])))
+        return res#[list(l) for l in res]
+        #return res
 
-        return [list(l) for l in res]
+    @staticmethod
+    def three_sum_2(nums, target):
+        """
+        3sum, second try, with list
+
+        x+y+z = target => y+z = w = target-x
+
+        PROBLEM: missed [0,0,0]
+        """
+        nums = sorted(nums) # O(n log n)
+        print("sorted nums = {}".format(nums))
+        N = len(nums)
+
+        #res = set()
+        res = []
+        for i, x in enumerate(nums):
+            # make sure j is not repeating
+            # made use of sorted nums
+            if nums[i] == nums[i-1] and i > 0:
+                continue
+            
+            w = target - x
+            start = i+1
+            end = N - 1
+            while end > start:
+                if nums[start]+nums[end] > w:
+                    end -= 1
+                    while end > start and nums[end] == nums[end-1]:
+                        end -= 1
+                    print(end)
+                if nums[start]+nums[end] < w:
+                    start += 1
+                    while end > start and nums[start-1] == nums[start]:
+                        start += 1
+                #print(start, end, nums[start], nums[end])
+                if end > start and nums[start]+nums[end] == w:
+                    #entry = tuple(sorted([nums[start],nums[end],x]))
+                    entry = [x, nums[start], nums[end]]
+                    print(entry)
+                    #res.add(entry)
+                    res.append(entry)
+
+                    end -= 1
+                    start += 1
+
+        res_uniq = list(set([tuple(l) for l in res]))
+        #return [list(l) for l in res]
+        return res_uniq
 
 
     @staticmethod
@@ -82,11 +139,16 @@ class Solution:
 def main():
     """main routine
     """
-    args = {'nums' : [-1, 0, 1, 0, 2, -2, 0],
+    #args = {'nums' : [-1, 0, 1, 0, 2, -2, 0],
+    #        'target' : 0}
+    args = {'nums' : [-4, -3, -3, -3, -3, 1, 1, 1, 2, 2, 3, 3, 3, 6],
             'target' : 0}
+    #args = {'nums' : [-1, 0, 1, 2, -1, -4],
+    #        'target' : 0}
 
     sol = Solution()
     print(sol.three_sum(**args))
+    print(sol.three_sum_2(**args))
 
 
 if __name__ == '__main__':
