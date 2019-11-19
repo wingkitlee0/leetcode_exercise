206b4c21c404de208926c8812493077a90d4d9ee
Kit Lee <wklee4993@gmail.com>
2019-11-19 00:03:29 -0500

    update

--- ex206_reverse_linked_list/sol1.py
+++ ex206_reverse_linked_list/sol1.py
@@ -14,39 +14,52 @@ class ListNode:
                 node = node.next
             else:
                 break
-
         print()
+
+    @staticmethod
+    def list2nodes(lst):
+        if lst is None or lst == []:
+            return None
+        head = ListNode(inp.pop(0))
+        node = head
+        while inp != []:
+            node.next = ListNode(inp.pop(0))
+            node = node.next
+        return head
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         """
         recursive version
         """
-        ListNode.print_values(head)
-
         if head is None:
             return None
-        if head.next is None:
-            return ListNode(head.val)
 
-        print("calling append {} to {}".format(head.val, head.next.val))
-        
-        tmp = self.reverseList(head.next)
-        head.next.next = ListNode(head.val)
+        def append(node, head):
+            """
+            return new_head, new_tail
+            """
+            node.next = None
 
-        ListNode.print_values(tmp)
-        return tmp
+            if head is None:
+                return node, node
+            else:
+                if head.next is None:
+                    head.next = node
+                    return head, node
+                else:
+                    new_head, tmp_tail = append(head, head.next)
+                    tmp_tail.next = node
+                    return new_head, node
+
+        new_head, _ = append(head, head.next)
+        return new_head
 
 if __name__ == '__main__':
 
     inp = [1, 2, 3, 4]
 
-    head = ListNode(inp.pop(0))
-    node = head
-    while inp != []:
-        node.next = ListNode(inp.pop(0))
-        node = node.next
-        
+    head = ListNode.list2nodes(inp)    
     ListNode.print_values(head)
 
     sol = Solution()
