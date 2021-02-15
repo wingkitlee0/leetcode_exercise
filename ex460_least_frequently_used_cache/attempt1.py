62c34721393c70b921db26a496291f323137deb7
Kit Lee <wklee4993@gmail.com>
2021-02-15 13:22:57 -0500

    refactoring

--- ex460_least_frequently_used_cache/attempt1.py
+++ ex460_least_frequently_used_cache/attempt1.py
@@ -1,29 +1,41 @@
 from collections import defaultdict
 
-class LFUCache:
 
+class LFUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.counter = defaultdict(int) # key: count
-        self.data = dict() # count: {key: value}
-        
+        self.counter = defaultdict(int)  # key: count
+        self.data = dict()  # count: {key: value}
+
+    def pop_data(self, count, key=None, left=False):
+        """
+        If left is True, pop the first entry in the dictionary (key is ignored)
+        """
+        value = self.data[count].pop(key)
+        if len(self.data[count]) == 0:
+            del self.data[count]
+        return value
+
+    def popleft_data(self, count):
+        key, value = next(iter(self.data[count].items()))
+        del self.data[count][key]
+        return key, value
+
+    def add_data(self, count, key, value):
+        if count in self.data:
+            self.data[count][key] = value
+        else:
+            self.data[count] = {key: value}
+
     def get(self, key: int) -> int:
         if key not in self.counter:
             return -1
 
         count = self.counter[key]
-        value = self.data[count].pop(key)
+        value = self.pop_data(count, key)
 
-        # clean up
-        if len(self.data[count]) == 0:
-            del self.data[count]
-
-        if count+1 in self.data:
-            self.data[count+1][key] = value
-        else:
-            self.data[count+1] = {key: value}
-        # update counter
-        self.counter[key] += 1
+        self.counter[key] += 1  # update counter
+        self.add_data(count + 1, key, value)
         return value
 
     def put(self, key: int, value: int) -> None:
@@ -31,20 +43,11 @@ class LFUCache:
             return
         if key in self.counter:
             # no deletion
-            count = self.counter[key] # current count
+            count = self.counter[key]  # current count
             self.counter[key] += 1
 
-            # pop the old value from the data dict
-            self.data[count].pop(key)
-            # cleanup
-            if len(self.data[count]) == 0:
-                del self.data[count]
-                
-            # put the new value
-            if count+1 not in self.data:
-                self.data[count+1] = {key: value}
-            else:
-                self.data[count+1][key] = value
+            _ = self.pop_data(count, key)  # pop the old value from the data dict
+            self.add_data(count + 1, key, value)  # put the new value
 
         else:
             if len(self.counter) == self.capacity:
@@ -53,21 +56,14 @@ class LFUCache:
                 print(f"min_count: {min_count}")
 
                 # pop the least frequent and least recent key-value pair
-                min_key, min_value = next(iter(self.data[min_count].items()))
-                # cleanup
-                del self.data[min_count][min_key]
-                if len(self.data[min_count]) == 0:
-                    del self.data[min_count]
+                min_key, min_value = self.popleft_data(min_count)
                 del self.counter[min_key]
 
             # add new data
             self.counter[key] = 1
-            if 1 not in self.data:
-                self.data[1] = {key: value}
-            else:
-                self.data[1][key] = value
+            self.add_data(1, key, value)
 
-    
+
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
 # param_1 = obj.get(key)
